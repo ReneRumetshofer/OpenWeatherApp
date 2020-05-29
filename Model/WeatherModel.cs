43207a15549e6fe23029fa2325fd11bf2d7e046b
ReneRumetshofer <51208355+ReneRumetshofer@users.noreply.github.com>
2020-05-29 22:31:45 +0200

    initial commit

---
+++ Model/WeatherModel.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherAPITest.Model
+{
+    public class WeatherModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Name { get; set; }
+        public int CityID { get; set; }
+        public string CountryTag { get; set; }
+
+        public int WeatherID { get; set; }
+        public string WeatherMain { get; set; }        
+        public string WeatherDescription { get; set; }
+        public double Temperature { get; set; }
+        public double FeltTemperature { get; set; }
+        public double MinTemperature { get; set; }
+        public double MaxTemperature { get; set; }
+        public double Pressure { get; set; }
+        public double Humidity { get; set; }
+        public double Visibility { get; set; }
+        public double WindSpeed { get; set; }
+        public double WindDegree { get; set; }
+        public double CloudsAll { get; set; }
+        
+        public long DataTimestamp { get; set; }
+        public long SunriseTimestamp { get; set; }
+        public long SunsetTimestamp { get; set; }
+
+        public static WeatherModel FromJson(JObject json)
+        {
+            return new WeatherModel() { 
+                Latitude = double.Parse(json["coord"]["lat"].ToString()),
+                Longitude = double.Parse(json["coord"]["lon"].ToString()),
+                Name = json["name"].ToString(),
+                CityID = int.Parse(json["sys"]["id"].ToString()),
+                CountryTag = json["sys"]["country"].ToString(),
+
+                WeatherID = int.Parse(json["weather"][0]["id"].ToString()),
+                WeatherMain = json["weather"][0]["main"].ToString(),
+                WeatherDescription = json["weather"][0]["description"].ToString(),
+                Temperature = double.Parse(json["main"]["temp"].ToString()),
+                FeltTemperature = double.Parse(json["main"]["feels_like"].ToString()),
+                MinTemperature = double.Parse(json["main"]["temp_min"].ToString()),
+                MaxTemperature = double.Parse(json["main"]["temp_max"].ToString()),
+                Pressure = double.Parse(json["main"]["pressure"].ToString()),
+                Humidity = double.Parse(json["main"]["humidity"].ToString()),
+                Visibility = double.Parse(json["visibility"].ToString()),
+                WindSpeed = double.Parse(json["wind"]["speed"].ToString()),
+                WindDegree = double.Parse(json["wind"]["deg"].ToString()),
+                CloudsAll = double.Parse(json["clouds"]["all"].ToString()),
+
+                DataTimestamp = long.Parse(json["dt"].ToString()),
+                SunriseTimestamp = long.Parse(json["sys"]["sunrise"].ToString()),
+                SunsetTimestamp = long.Parse(json["sys"]["sunset"].ToString())
+            };
+        }
+
+    }
+}
---
+++ ViewModel/WeatherViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Input;
+using WeatherAPITest.Model;
+
+namespace WeatherAPITest.ViewModel
+{
+    class WeatherViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public WeatherModel Model{ get; set; }
+
+        public WeatherViewModel()
+        {
+            var client = new RestClient("http://api.openweathermap.org/data/2.5/");
+            var request = new RestRequest("weather?q=Amstetten,AT&appid=a8dfc048e5d6fc6a63616f39cb2c312f", DataFormat.Json);
+            var response = client.Get(request);
+
+            Console.WriteLine(JObject.Parse(response.Content));
+            Model = WeatherModel.FromJson(JObject.Parse(response.Content));
+        }
+    }
+}
